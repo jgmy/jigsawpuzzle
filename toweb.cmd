921bf0e5d18c62741ec5c4d0fd12f105639945f9
Jose Moya <josemoya-git@apagada.com>
2013-05-06 21:07:19 +0200

    toweb.cmd now changes the openwebapp manifest to reflect its location at github web.

--- toweb.cmd
+++ toweb.cmd
@@ -1,1 +1,3 @@
 for %%A in (chrome, openwebapp, webapp) do xcopy /S %%A ..\jigsawpuzzle-web\%%A 
+
+type openwebapp\manifest.webapp|sed -e "s!/varios/jigsaw_owapp/!/jigsawpuzzle/openwebapp/!">..\jigsawpuzzle-web\openwebapp\manifest.webapp
